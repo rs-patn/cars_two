886f20c2ffbb5249d1fbadcfa0d86331b05779ab
rs-patn <patrick.naegele@rackspace.com>
2022-05-03 11:19:20 -0400

    create cars info table

---
+++ migrations/V1.0.0_20220503151426__createCars_InfoTable.sql
@@ -0,0 +1,14 @@
+﻿SET NUMERIC_ROUNDABORT OFF
+GO
+SET ANSI_PADDING, ANSI_WARNINGS, CONCAT_NULL_YIELDS_NULL, ARITHABORT, QUOTED_IDENTIFIER, ANSI_NULLS ON
+GO
+PRINT N'Creating [dbo].[car_info]'
+GO
+CREATE TABLE [dbo].[car_info]
+(
+[make] [varchar] (50) NULL,
+[model] [varchar] (50) NULL,
+[modelYear] [smallint] NULL,
+[color] [varchar] (25) NULL
+)
+GO
---
+++ schema-model/Tables/dbo.car_info.sql
@@ -0,0 +1,8 @@
+CREATE TABLE [dbo].[car_info]
+(
+[make] [varchar] (50) NULL,
+[model] [varchar] (50) NULL,
+[modelYear] [smallint] NULL,
+[color] [varchar] (25) NULL
+)
+GO
