d4b357e87bbe02a5ad136196b2e39b272a731c7d
rs-patn <patrick.naegele@rackspace.com>
2022-05-03 11:33:56 -0400

    adding Mileage column

--- schema-model/Tables/dbo.car_info.sql
+++ schema-model/Tables/dbo.car_info.sql
@@ -3,6 +3,7 @@ CREATE TABLE [dbo].[car_info]
 [make] [varchar] (50) NULL,
 [model] [varchar] (50) NULL,
 [modelYear] [smallint] NULL,
-[color] [varchar] (25) NULL
+[color] [varchar] (25) NULL,
+[mileage] [int] NULL
 )
 GO
